2ddbac702e8e7eb29cdf6b9c59792ec96eb06fc8
AndreeaNegru <andreeanegru1982@gmail.com>
2025-08-06 14:54:42 +0300

    done the POST

--- src/Features/Books/AddBooks.tsx
+++ src/Features/Books/AddBooks.tsx
@@ -1,14 +1,26 @@
 import { z } from "zod/v4";
 import { validateForm, type ValidationErrors } from "../../utils/validations";
 import { useState } from "react";
+import { useAuthContext } from "../Auth/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const validationSchema = z.object({
   title: z.string().min(1, "Title is required."),
   author: z.string().min(1, "Author is required."),
-  year: z.coerce.number("Please specify a valid release year.").int("Please specify a valid release year.").min(1000, "Please specify a valid release year.").max(9999, "Please specify a valid release year."),
+  year: z.coerce
+    .number("Please specify a valid release year.")
+    .int("Please specify a valid release year.")
+    .min(1000, "Please specify a valid release year.")
+    .max(9999, "Please specify a valid release year."),
   genre: z.string().min(1, "Genre is required."),
   cover: z.url("Please enter a valid URL for the book's cover."),
-  rating: z.coerce.number("Please select rating").min(1, "Please select rating").max(5, "Please select rating"),
+  rating: z.coerce
+    .number("Please select rating")
+    .min(1, "Please select rating")
+    .max(5, "Please select rating"),
   review: z.string().optional(),
 });
 
@@ -28,42 +40,125 @@ export function AddBooks() {
   >>(null);
   const [defaultValues, setDefaultValues] = useState(initialDefaultValues);
 
-  async function handleAddBook(formData: FormData) {
-    const values = Object.fromEntries(formData.entries());
-    const errors = validateForm(values, validationSchema);
+  const { user, accessToken } = useAuthContext();
+  const navigate = useNavigate();
 
-    console.log(values);
+  function handleInputChange(
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) {
+    if (!errors) return;
+
+    const form = e.currentTarget.form;
+    if (!form) return;
+
+    const formData = new FormData(form);
+    const values = Object.fromEntries(formData.entries());
+    const newErrors = validateForm(values, validationSchema);
+
+    setErrors(newErrors);
+  }
+
+  async function handleAddBook(formData: FormData) {
+    const rawValues: Record<string, FormDataEntryValue | FormDataEntryValue[]> =
+      Object.fromEntries(formData.entries());
+
+    const errors = validateForm(rawValues, validationSchema);
 
     if (errors) {
       setErrors(errors);
-      setDefaultValues(values as typeof defaultValues);
+      setDefaultValues(rawValues as typeof defaultValues);
       return;
     }
     setErrors(null);
     setDefaultValues(initialDefaultValues);
+
+    const newBookData = {
+      title: String(rawValues.title),
+      author: String(rawValues.author),
+      year: Number(rawValues.year),
+      genre: String(rawValues.genre),
+      cover: String(rawValues.cover),
+      userId: Number(user!.id),
+      ratings: [Number(rawValues.rating)],
+      reviews: rawValues.review ? [String(rawValues.review)] : [],
+    };
+
+    await fetch(`${apiUrl}/books`, {
+      method: "POST",
+      body: JSON.stringify(newBookData),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    toast.success(
+      `You have successfully added a new book: "${newBookData.title}" by "${newBookData.author}"`
+    );
+    navigate("/books");
   }
   return (
-    <form className="brandForm" action={handleAddBook}>
+    <form
+      className="brandForm"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        handleAddBook(formData);
+      }}
+    >
       <h1 className="fullWidth">Add Books</h1>
 
       <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" defaultValue={defaultValues.title}/>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        defaultValue={defaultValues.title}
+        onChange={handleInputChange}
+      />
       {errors?.title && <p className="fieldError">{errors.title[0]}</p>}
 
       <label htmlFor="author">Author</label>
-      <input type="text" id="author" name="author" defaultValue={defaultValues.author}/>
+      <input
+        type="text"
+        id="author"
+        name="author"
+        defaultValue={defaultValues.author}
+        onChange={handleInputChange}
+      />
       {errors?.author && <p className="fieldError">{errors.author[0]}</p>}
 
       <label htmlFor="year">Year</label>
-      <input type="text" id="year" name="year" inputMode="numeric" defaultValue={defaultValues.year}/>
+      <input
+        type="text"
+        id="year"
+        name="year"
+        inputMode="numeric"
+        defaultValue={defaultValues.year}
+        onChange={handleInputChange}
+      />
       {errors?.year && <p className="fieldError">{errors.year[0]}</p>}
 
       <label htmlFor="genre">Genre</label>
-      <input type="text" id="genre" name="genre" defaultValue={defaultValues.genre}/>
+      <input
+        type="text"
+        id="genre"
+        name="genre"
+        defaultValue={defaultValues.genre}
+        onChange={handleInputChange}
+      />
       {errors?.genre && <p className="fieldError">{errors.genre[0]}</p>}
 
       <label htmlFor="cover">Cover</label>
-      <input type="url" id="cover" name="cover" defaultValue={defaultValues.cover}/>
+      <input
+        type="url"
+        id="cover"
+        name="cover"
+        defaultValue={defaultValues.cover}
+        onChange={handleInputChange}
+      />
       {errors?.cover && <p className="fieldError">{errors.cover[0]}</p>}
 
       <span className="label alignTop">Rating</span>
@@ -72,14 +167,24 @@ export function AddBooks() {
         {[1, 2, 3, 4, 5].map((num) => (
           <label htmlFor="rating" key={num}>
             {num}
-            <input type="radio" name="rating" value={num} defaultChecked={defaultValues.rating === String(num)}/>
-            
+            <input
+              type="radio"
+              name="rating"
+              value={num}
+              defaultChecked={defaultValues.rating === String(num)}
+              onChange={handleInputChange}
+            />
           </label>
         ))}
-        {errors?.rating&& <p className="fieldError">{errors.rating[0]}</p>}
+        {errors?.rating && <p className="fieldError">{errors.rating[0]}</p>}
       </fieldset>
       <label htmlFor="review">Review</label>
-      <textarea id="review" name="review" defaultValue={defaultValues.review}/>
+      <textarea
+        id="review"
+        name="review"
+        defaultValue={defaultValues.review}
+        onChange={handleInputChange}
+      />
 
       <button type="submit" className="btn btnFormAction btnCenter">
         Add book
